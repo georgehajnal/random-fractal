a05883ae84e65c7a0e93d551da92d6cb3ae40d56
georgehajnal <128971944+georgehajnal@users.noreply.github.com>
2023-03-26 14:09:20 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <windows.h>
+#include <ctime>
+//cat de mare sa fie ecranul
+#define scale 1
+//cate celule au liniile si coloanele matricei
+#define siz 50
+using namespace std;
+HDC dng = GetDC(0);
+int screen[siz][siz];
+int i,j,done,n;
+void refresh() {
+    for(i=0;i<siz*scale;i++) {
+        for(j=0;j<siz*scale;j++){
+            SetPixel(dng,i,j,RGB(screen[i/scale][j/scale],screen[i/scale][j/scale],screen[i/scale][j/scale]));
+        }
+    }
+}
+int main()
+{
+    srand(time(NULL));
+    Sleep(1000);
+    for (i=0;i<siz;i++) {
+        screen[i][0]=screen[i][siz-1]=0;
+        screen[0][i]=screen[siz-1][i]=0;
+    }
+    for(i=1;i<siz-1;i++) {
+        for(j=1;j<siz-1;j++){
+            screen[i][j] = rand()%255;
+        }
+    }
+    refresh();
+    while(1){
+        n++;
+        for(i=1;i<siz-1;i++){
+            for(j=1;j<siz-1;j++){
+                screen[i][j] = (screen[i+1][j] + screen[i][j+1] + screen[i-1][j] + screen[i][j-1] + screen[i+1][j+1] + screen[i-1][j+1] + screen[i-1][j-1] + screen[i+1][j-1] + rand()%255)/8;
+
+            }
+        }
+        if (n%1000==0) {refresh(); cout<<n<<endl;}
+    }
+    return 0;
+}
